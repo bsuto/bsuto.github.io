e1c218fd913e4bf08c3b7e90f733598c6cfb1796
Brian Sutorius <brian.sutorius@gmail.com>
2024-08-26 15:54:37 -0400

    mini tour map!

---
+++ minitour/map/pin-data.js
@@ -0,0 +1,14 @@
+const minis = [
+['Angels Flight','The shortest railway in the world','34.051361044044306','-118.25023791104549'],
+['Cuexcomate','The smallest volcano in the world','19.070804492069325','-98.23499836471427'],
+['Ebenezer Place','The shortest street in the world','58.44119677893448','-3.0941699706307797'],
+['Hess Estate Triangle','The smallest piece of private property in New York City','40.733524740478366','-74.00306641165682'],
+['El Marco Bridge','The smallest international bridge in the world','39.18088444474428','-7.169827983201339'],
+['Mill Ends Park','The smallest municipal park in the world','45.516205469861','-122.67324511656824'],
+['Newby-McMahon Building','The smallest skyscraper in the world','33.914374323565916','-98.48978187926451'],
+['Ochopee Post Office','The smallest post office in the United States','25.901259056057157','-81.29733344355164'],
+['Prada Marfa','The smallest Prada store','30.603491205945232','-104.51852413743651'],
+['Somerset Bridge','The smallest drawbridge in the world','32.278548723383736','-64.87670024721795'],
+['Sutter Buttes','The smallest mountain range in the world','39.22427459636337','-121.8150327463446'],
+['Tom Thumb Island','The smallest of the Thousand Islands','44.35767781841905','-75.98563488954517']
+];
